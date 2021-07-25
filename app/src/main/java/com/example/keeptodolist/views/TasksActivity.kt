2e2844786c9471223382b423024f067dee6161d6
Menna Saber <51786367+mennasaber@users.noreply.github.com>
2021-07-25 22:53:27 +0200

    implement Add task feature

--- app/src/main/java/com/example/keeptodolist/views/TasksActivity.kt
+++ app/src/main/java/com/example/keeptodolist/views/TasksActivity.kt
@@ -3,6 +3,8 @@ package com.example.keeptodolist.views
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -11,33 +13,41 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.keeptodolist.R
 import com.example.keeptodolist.adapters.TasksAdapter
-import com.example.keeptodolist.viewmodels.KeepViewModel
 import com.example.keeptodolist.data.Task
 import com.example.keeptodolist.utilities.HandleTaskOnClick
+import com.example.keeptodolist.viewmodels.KeepViewModel
 import de.hdodenhof.circleimageview.CircleImageView
 
-class TasksActivity : AppCompatActivity(),View.OnClickListener {
+class TasksActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var keepViewModel: KeepViewModel
+    private lateinit var addTaskEditText: EditText
+    private var listId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
 
-        val listId = intent.getIntExtra("listId", 0)
+        listId = intent.getIntExtra("listId", 0)
         val listName = intent.getStringExtra("listName")
         val listColor = intent.getIntExtra("listColor", 0)
         val listTextView = findViewById<TextView>(R.id.listTitle_textView)
         val listColorIcon = findViewById<CircleImageView>(R.id.listIcon_imageView)
         val recyclerView = findViewById<RecyclerView>(R.id.tasks_recyclerView)
         val completedRecyclerView = findViewById<RecyclerView>(R.id.completedTasks_recyclerView)
-        val completeTextView =  findViewById<TextView>(R.id.completeTextView)
+        val completeTextView = findViewById<TextView>(R.id.completeTextView)
         val backButton = findViewById<Button>(R.id.backButton)
+        val addButton = findViewById<ImageView>(R.id.addTask_button)
         var tasksAdapter: TasksAdapter
         var completedTasksAdapter: TasksAdapter
+        addTaskEditText = findViewById(R.id.addTask_editText)
 
         backButton.setOnClickListener(this)
-        listColorIcon.borderColor = ContextCompat.getColor(this, listColor)
+        addButton.setOnClickListener(this)
+
         listTextView.text = listName
+        listColorIcon.borderColor = ContextCompat.getColor(this, listColor)
+
         keepViewModel = ViewModelProvider(this)[KeepViewModel::class.java]
+
         completedRecyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -50,8 +60,8 @@ class TasksActivity : AppCompatActivity(),View.OnClickListener {
             recyclerView.adapter = tasksAdapter
         })
         keepViewModel.getCompletedTasks(listId).observe(this, { tasks ->
-            if(tasks.count()==0)
-               completeTextView.visibility= View.GONE
+            if (tasks.count() == 0)
+                completeTextView.visibility = View.GONE
             else
                 completeTextView.visibility = View.VISIBLE
             completedTasksAdapter = TasksAdapter(tasks, object : HandleTaskOnClick {
@@ -64,8 +74,21 @@ class TasksActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
+        when (v?.id) {
             R.id.backButton -> finish()
+            R.id.addTask_button -> {
+                if (addTaskEditText.text.toString().trim().isNotEmpty()) {
+                    keepViewModel.addTask(
+                        Task(
+                            0,
+                            addTaskEditText.text.toString().trim(),
+                            false,
+                            listId
+                        )
+                    )
+                    addTaskEditText.text.clear()
+                }
+            }
         }
     }
 }
